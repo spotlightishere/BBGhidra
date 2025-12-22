3005bef516e2c873c80292e637ed925dba402802
Spotlight <spotlight@joscomputing.space>
2025-12-21 20:46:50 -0600

    Mark OS and app image sections

--- src/main/java/space/joscomputing/BBGhidra/SFILoader.java
+++ src/main/java/space/joscomputing/BBGhidra/SFILoader.java
@@ -1,13 +1,20 @@
 package space.joscomputing.BBGhidra;
 
+import ghidra.app.util.MemoryBlockUtils;
 import ghidra.app.util.bin.ByteProvider;
 import ghidra.app.util.opinion.AbstractProgramWrapperLoader;
+import ghidra.app.util.opinion.LoadException;
 import ghidra.app.util.opinion.LoadSpec;
 import ghidra.app.util.opinion.LoaderTier;
+import ghidra.framework.store.LockException;
+import ghidra.program.model.address.Address;
+import ghidra.program.model.address.AddressOverflowException;
+import ghidra.program.model.address.AddressSpace;
 import ghidra.program.model.lang.LanguageCompilerSpecPair;
 import ghidra.program.model.listing.Program;
 import ghidra.util.exception.CancelledException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 import space.joscomputing.BBGhidra.formats.SFIHeader;
 
@@ -34,8 +41,56 @@ public class SFILoader extends AbstractProgramWrapperLoader {
 
     @Override
     protected void load(Program program, ImporterSettings settings) throws CancelledException, IOException {
-        // Load the bytes from 'settings.provider()' into the 'program'.
+        try (ByteProvider provider = settings.provider()) {
+            loadForReal(provider, program, settings);
+        } catch (IOException | AddressOverflowException | LockException e) {
+            throw new LoadException(e);
+        }
+    }
 
+    private void loadForReal(ByteProvider provider, Program program, ImporterSettings settings)
+            throws IOException, AddressOverflowException, LockException {
+        SFIHeader header = new SFIHeader(provider);
+        AddressSpace addressSpace = program.getAddressFactory().getDefaultAddressSpace();
+
+        // First, import OS as-is.
+        // OS will always be our base address.
+        Address osBaseAddress = addressSpace.getAddress(header.getOsBaseAddress());
+        program.setImageBase(osBaseAddress, true);
+
+        InputStream osImage = provider.getInputStream(header.getOsImageOffset());
+        MemoryBlockUtils.createInitializedBlock(
+                program,
+                false,
+                "os",
+                osBaseAddress,
+                osImage,
+                header.getOsSize(),
+                "",
+                null,
+                true,
+                true,
+                true,
+                null,
+                settings.monitor());
+
+        // Similarly, import app as-is.
+        Address appBaseAddress = addressSpace.getAddress(header.getAppBaseAddress());
+        InputStream appImage = provider.getInputStream(header.getAppImageOffset());
+        MemoryBlockUtils.createInitializedBlock(
+                program,
+                false,
+                "app",
+                appBaseAddress,
+                appImage,
+                header.getAppSize(),
+                "",
+                null,
+                true,
+                true,
+                true,
+                null,
+                settings.monitor());
     }
 
     @Override
--- src/main/java/space/joscomputing/BBGhidra/formats/SFIHeader.java
+++ src/main/java/space/joscomputing/BBGhidra/formats/SFIHeader.java
@@ -17,11 +17,16 @@ public class SFIHeader {
     private static final int SFI_TYPE_OS_IMAGE = 2;
 
     private static final long SFI_OS_MAGIC = 0xD7A82D1FL;
+    private static final long SFI_OS_HEADER_SIZE = 20;
+    private static final long SFI_APP_IMAGE_MAIN_OFFSET = 116;
     private static final long SFI_APP_MAGIC = 0xD7CC2D1FL;
     private static final long SFI_SIGNATURE_MAGIC = 0xD7C82D1FL;
 
+    private final long osImageOffset;
     private final long osBaseAddress;
     private final long osSize;
+
+    private final long appImageOffset;
     private final long appBaseAddress;
     private final long appSize;
 
@@ -66,12 +71,15 @@ public class SFIHeader {
         BinaryReader reader = new BinaryReader(provider, true);
 
         // Within our SFI header, we have fields like version information.
-        // Skip beyond this to the OS image, validating its magic.
+        // Our OS image contains its magic bytes beyond its ARMv6 reset vectors.
+        // Mark the true offset, but skip beyond this header.
+        //
         // This is:
         // - 20 bytes for header
         // - 4 bytes for (possibly?) image count
         // - 28 bytes for ARMv6 reset vectors
-        reader.setPointerIndex(SFI_HEADER_LENGTH + 4 + 28);
+        this.osImageOffset = SFI_HEADER_LENGTH;
+        reader.setPointerIndex(osImageOffset + 4 + 28);
 
         // TODO(spotlightishere): There may be multiple magic values for images.
         // This is based off of a BlackBerry Tour 9630's firmware.
@@ -82,7 +90,6 @@ public class SFIHeader {
 
         this.osBaseAddress = reader.readNextUnsignedIntExact();
         long osEndAddress = reader.readNextUnsignedInt();
-        this.osSize = osBaseAddress - osEndAddress;
 
         // TODO(spotlightishere): This is hacky!
         // We skip to the end of the OS base address,
@@ -90,16 +97,19 @@ public class SFIHeader {
         //
         // In our file, that's:
         // (20 bytes for SFI) + (20 for OS image header) + osSize.
-        final long signatureLength = SFI_HEADER_LENGTH + 20;
         //
         // Within our signature block, we can read the main application's
         // address. Its address is present 116 bytes in.
-        final long appMainInfoOffset = 116;
-        final long appMainAddressOffset = signatureLength + osSize + appMainInfoOffset;
+        final long osBinarySize = osEndAddress - osBaseAddress;
+        final long appMainAddressOffset =
+                SFI_HEADER_LENGTH + SFI_OS_HEADER_SIZE + osBinarySize + SFI_APP_IMAGE_MAIN_OFFSET;
         final long appMainAddress = reader.readUnsignedInt(appMainAddressOffset);
 
+        // We adjust OS size to match from its start to before address' beginning.
+        this.osSize = appMainAddress - osBaseAddress;
+
         // Finally, we must adjust the offset to adapt for our SFI's header length.
-        final long appImageOffset = SFI_HEADER_LENGTH + appMainAddress - osBaseAddress;
+        this.appImageOffset = SFI_HEADER_LENGTH + appMainAddress - osBaseAddress;
         reader.setPointerIndex(appImageOffset);
 
         long appHeaderMagic = reader.readNextUnsignedInt();
@@ -109,5 +119,29 @@ public class SFIHeader {
         this.appBaseAddress = reader.readNextUnsignedInt();
         long appEndAddress = reader.readNextUnsignedInt();
         this.appSize = appEndAddress - appBaseAddress;
+    }
+
+    public long getOsImageOffset() {
+        return osImageOffset;
+    }
+
+    public long getOsSize() {
+        return osSize;
+    }
+
+    public long getOsBaseAddress() {
+        return osBaseAddress;
+    }
+
+    public long getAppImageOffset() {
+        return appImageOffset;
+    }
+
+    public long getAppSize() {
+        return appSize;
+    }
+
+    public long getAppBaseAddress() {
+        return appBaseAddress;
     }
 }
